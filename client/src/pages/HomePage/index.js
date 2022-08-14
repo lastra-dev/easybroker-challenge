67bf280e8077cb18e31abbbafdd44ea4fe969379
Oscar Lastra <oscar.glastra@gmail.com>
2022-08-14 17:54:19 -0500

    fix: Change CardMedia to Box for responsiveness

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,40 +1,37 @@
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import CardMedia from "@mui/material/CardMedia";
 import Box from "@mui/material/Box";
 
 const HomePage = () => {
   return (
-    <Stack justifyContent="center" alignItems="center" spacing={4}>
-      <Typography mt={4} align="center" variant="h1" fontWeight={500}>
-        A{" "}
-        <Box display="inline" sx={{ color: "primary.main" }}>
-          Good
-        </Box>{" "}
-        House
-        <br />
-        for a good life
-      </Typography>
-      <Typography align="center" variant="h6" fontWeight={500} color="primary">
-        Checkout your dream property now!
-      </Typography>
-      <Button href="/properties" variant="contained" size="large">
-        Get Started
-      </Button>
-      <Box>
-        <CardMedia
+    <Box padding={2}>
+      <Stack justifyContent="center" alignItems="center" spacing={4}>
+        <Typography mt={4} align="center" variant="h1" fontWeight={500}>
+          A{" "}
+          <Box display="inline" sx={{ color: "primary.main" }}>
+            Good
+          </Box>{" "}
+          House
+          <br />
+          for a good life
+        </Typography>
+        <Typography align="center" variant="h6" fontWeight={500} color="primary">
+          Checkout your dream property now!
+        </Typography>
+        <Button href="/properties" variant="contained" size="large">
+          Get Started
+        </Button>
+        <Box
           component="video"
-          height="400"
-          image="/static/videos/video1.mp4"
-          alt="Houses recording"
+          src="/static/videos/video1.mp4"
           autoPlay
           muted
           loop
-          sx={{ borderRadius: "4%", padding: "0 16px" }}
+          sx={{ borderRadius: "8px", width: "900px", maxWidth: "100%" }}
         />
-      </Box>
-    </Stack>
+      </Stack>
+    </Box>
   );
 };
 
