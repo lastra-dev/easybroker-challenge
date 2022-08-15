3827e3ddc3f3d9e059caf5b7ce53d8be8f228718
Oscar Lastra <oscar.glastra@gmail.com>
2022-08-15 02:59:00 -0500

    chore: Add docker-compose

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // IP for development only.
-  // baseURL: "http://192.168.0.10:8080",
-  baseURL: "http://localhost:4000"
+  baseURL: `http://localhost:8080`
 });
 
 export default api;
